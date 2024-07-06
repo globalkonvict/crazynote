d6f4e673d1400c14aae7dbd671494416fe337a29
Sarthak Dwivedi <sarthakwarlock@gmail.com>
2024-07-06 15:53:23 +0530

    - Initial Commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Crazynote - Customizable code flattener
+
+## Usage
+
+1. Install crazynote
+
+```sh
+npm crazynote
+```
+
+2. Genrate default config in your project root by running: \
+
+```sh
+crazynote --init
+```
+
+3. Run the final command:
+
+```sh
+crazynote
+```
+
+4. It will generate a output.md
+5. Change config in `crazyconfig.json` if you need something more.
---
+++ bin/crazynote.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+
+const { program } = require("commander");
+const { generateMarkdown } = require("../lib/generateMarkdown");
+const { parseConfig } = require("../lib/parseConfig");
+const { generateDefaults } = require("../lib/generateDefaultConfig");
+const path = require("path");
+
+program
+  .version("1.0.0")
+  .description("CLI tool to generate Markdown documentation from project files")
+  .option(
+    "-c, --config <path>",
+    "Path to configuration file",
+    "crazyconfig.json"
+  )
+  .option("--init", "Initialize a configuration file with defaults")
+  .option(
+    "--template <name>",
+    "Specify a template to use (minimalist, detailed_explorer, colorful_insights)"
+  )
+  .parse(process.argv);
+
+const options = program.opts();
+
+(async () => {
+  if (options.init) {
+    const templateName = options.template || "minimalist";
+    generateDefaults(templateName);
+  } else {
+    const configPath = path.resolve(process.cwd(), options.config);
+    const config = parseConfig(configPath);
+    await generateMarkdown(config);
+  }
+})();
